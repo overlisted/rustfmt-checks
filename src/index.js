bc9470a9a53cd547fd1ad62dd0801bd1c001179f
Ilya Maximov <mail@overlisted.net>
2021-04-14 22:06:35 +0200

    refactor using @octokit/app.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import express from "express";
-import { createAppAuth, createOAuthUserAuth } from "@octokit/auth-app";
-import { Octokit } from "@octokit/core";
+import { App, createNodeMiddleware } from "@octokit/app";
 import { createRun, beginRun, finishRun } from "./checkrun.js";
 import { checkProject, checkRustfmt, formatProject } from "./rustfmt.js";
 import { config } from "dotenv";
 import { readFile } from "fs/promises";
 import { cloneRepo, pull, pushChanges } from "./git.js";
 import exists from "./exists.js";
+import { createServer } from "http";
+import { createOAuthUserAuth } from "@octokit/auth-app";
+import { Octokit } from "@octokit/core";
 
 config();
+
+const logIntoInstallation = async (octokit, id) => {
+  const credentials = await octokit.auth({
+    type: "installation",
+    installationId: id
+  });
+
+  return [new Octokit({ authStrategy: createOAuthUserAuth, auth: credentials }), credentials.token];
+}
 
 const main = async () => {
   if(!checkRustfmt()) {
@@ -17,119 +27,91 @@ const main = async () => {
     return;
   }
 
-  const app = express();
+  const github = new App({
+    appId: process.env.APP_ID,
+    privateKey: await readFile("private-key.pem").then(buffer => buffer.toString()),
+    clientId: process.env.CLIENT_ID,
+    oauth: {
+      clientSecret: process.env.CLIENT_SECRET
+    },
+    webhooks: {
+      secret: process.env.WEBHOOK_SECRET
+    }
+  });
 
-  app.use(express.json());
+  github.webhooks.on(
+    ["check_suite.requested", "check_suite.rerequested", "check_run.rerequested"],
+    async ({ octokit, payload }) => {
+      const revision = payload.check_run?.head_sha ?? payload.check_suite?.head_sha;
 
-  app.post("/github", async (req, res) => {
-    const { action, repository, check_run, check_suite, installation, requested_action } = req.body;
-    const revision = check_run?.head_sha ?? check_suite?.head_sha;
-    const octokit = new Octokit({
-      authStrategy: createAppAuth,
-      auth: {
-        appId: process.env.APP_ID,
-        privateKey: await readFile("private-key.pem").then(buffer => buffer.toString()),
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET
-      }
-    });
-    const credentials = await octokit.auth({
-      type: "installation",
-      installationId: installation.id
-    });
-    const installationOctokit = new Octokit({ authStrategy: createOAuthUserAuth, auth: credentials });
+      await createRun(octokit, payload.repository, revision);
+  });
 
-    switch(req.header("X-GitHub-Event")) {
-      case "check_suite": {
-        switch(action) {
-          case "rerequested":
-          case "requested": {
-            await createRun(installationOctokit, repository, revision);
+  github.webhooks.on("check_run.created", async ({ payload }) => {
+    const [octokit, token] = await logIntoInstallation(github.octokit, payload.installation.id);
+    const revision = payload.check_run?.head_sha ?? payload.check_suite?.head_sha;
 
-            break;
-          }
+    console.log("== check run ==");
+    console.log("starting check run");
+    await beginRun(octokit, payload.repository, payload.check_run);
+
+    const path = `./repos/${payload.repository.full_name}`;
+    console.log("cloning");
+    await cloneRepo(token, path, payload.repository, revision);
+
+    console.log("checking rustfmt");
+    const [conclusion, output, actions] = !await exists(`${path}/Cargo.toml`)
+      ? [
+        "skipped",
+        {
+          summary: "The repository doesn't appear to use Cargo",
+          text: "This can't check repositories without a Cargo.toml in the root directory."
         }
+      ]
+      : checkProject(path)
+        ? ["success", { summary: "Rustfmt has found no formatting errors" }, []]
+        : [
+          "failure",
+          {
+            summary: "Rustfmt has found formatting errors. Fix the errors by running rustfmt in your project.",
+          },
+          [{ label: "Reformat", description: "Run rustfmt for you", identifier: "run_rustfmt" }]
+        ];
 
-        break;
-      }
-      case "check_run": {
-        switch(action) {
-          case "rerequested": {
-            await createRun(installationOctokit, repository, revision);
+    output.title = "rustfmt"
 
-            break;
-          }
-          case "created": {
-            console.log("== check run ==");
-            console.log("starting check run");
-            await beginRun(installationOctokit, repository, check_run);
+    console.log("submitting the result");
+    await finishRun(octokit, payload.repository, payload.check_run, conclusion, output, actions);
+  });
 
-            const path = `./repos/${repository.full_name}`;
-            console.log("cloning");
-            await cloneRepo(credentials.token, path, repository, revision);
+  github.webhooks.on("check_run.requested_action", async ({ octokit, payload }) => {
+    const [, token] = await logIntoInstallation(github.octokit, payload.installation.id);
 
-            console.log("checking rustfmt");
-            const [conclusion, output, actions] = !await exists(`${path}/Cargo.toml`)
-              ? [
-                  "skipped",
-                  {
-                    summary: "The repository doesn't appear to use Cargo",
-                    text: "This can't check repositories without a Cargo.toml in the root directory."
-                  }
-                ]
-              : checkProject(path)
-              ? ["success", { summary: "Rustfmt has found no formatting errors" }, []]
-              : [
-                  "failure",
-                  {
-                    summary: "Rustfmt has found formatting errors. Fix the errors by running rustfmt in your project.",
-                  },
-                  [{ label: "Reformat", description: "Run rustfmt for you", identifier: "run_rustfmt" }]
-              ];
+    switch(payload.requested_action.identifier) {
+      case "run_rustfmt": {
+        const path = `./repos/${payload.repository.full_name}`;
 
-            output.title = "rustfmt"
-
-            console.log("submitting the result");
-            await finishRun(installationOctokit, repository, check_run, conclusion, output, actions);
-
-            break;
-          }
-          case "requested_action": {
-            switch(requested_action.identifier) {
-              case "run_rustfmt": {
-                const path = `./repos/${repository.full_name}`;
-
-                console.log("== check run action ==");
-                console.log("updating the repo");
-                await pull(credentials.token, path, repository, check_run.check_suite.head_branch);
-                console.log("running rustfmt");
-                await formatProject(path);
-                console.log("pushing");
-                await pushChanges(
-                  credentials.token,
-                  path,
-                  repository,
-                  check_run.check_suite.head_branch,
-                  "Run rustfmt"
-                );
-
-                break;
-              }
-            }
-
-            break;
-          }
-        }
+        console.log("== check run action ==");
+        console.log("updating the repo");
+        await pull(token, path, payload.repository, payload.check_run.check_suite.head_branch);
+        console.log("running rustfmt");
+        await formatProject(path);
+        console.log("pushing");
+        await pushChanges(
+          token,
+          path,
+          payload.repository,
+          payload.check_run.check_suite.head_branch,
+          "Run rustfmt"
+        );
 
         break;
       }
     }
-
-    res.status(200);
-    res.end()
   });
 
-  app.listen(process.env.PORT);
+  createServer(createNodeMiddleware(github)).listen(process.env.PORT);
+
   console.info("im running")
 }
 
