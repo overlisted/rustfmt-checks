64ee570c104f1b338e14de19b289e49e1b059520
Ilya Maximov <mail@overlisted.net>
2021-07-22 01:48:13 +0200

    stop relying on PATH to run rustup executables

--- src/rustfmt.js
+++ src/rustfmt.js
@@ -1,9 +1,14 @@
 import { spawnSync, execSync } from "child_process";
 
+const commands = {
+  cargo: `${process.env.HOME}/.cargo/bin/cargo`,
+  rustfmt: `${process.env.HOME}/.cargo/bin/rustfmt`
+};
+
 export const checkRustfmt = () => spawnSync("rustfmt --version").error;
 
-export const formatCode = text => execSync("rustfmt", { input: text });
-export const formatFile = path => execSync(`rustfmt ${path}`);
-export const formatProject = directory => execSync("cargo fmt", { cwd: directory });
+export const formatCode = text => execSync(commands.rustfmt, { input: text });
+export const formatFile = path => execSync(`${commands.rustfmt} ${path}`);
+export const formatProject = directory => execSync(`${commands.cargo} fmt`, { cwd: directory });
 export const checkProject = directory =>
-  spawnSync("cargo", ["fmt", "--", "--check"], { cwd: directory }).status === 0;
+  spawnSync(commands.cargo, ["fmt", "--", "--check"], { cwd: directory }).status === 0;
